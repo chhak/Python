7f7e64751de7130b397422e2252a84143f38d6a5
aneklfae <anfekl@nael.com>
2020-06-24 17:07:59 +0900

    수업예제

---
+++ Ch08/8-2.py
@@ -0,0 +1,20 @@
+import pymysql as db
+
+conn = db.connect(host='192.168.44.46',
+                  user='chhak',
+                  password='1234',
+                  db='chhak',
+                  charset='utf8')
+
+cur = conn.cursor()
+cur.execute("SELECT * FROM `USER1`")
+
+for row in cur.fetchall():
+    print('-----------------')
+    print('아이디 :', row[0])
+    print('이름 :', row[1])
+    print('휴대폰 :', row[2])
+    print('나이 :', row[3])
+    print('-----------------')
+
+conn.close()
