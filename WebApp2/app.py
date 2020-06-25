831d459839382c608af924bad11f3b1d11f79d15
aneklfae <anfekl@nael.com>
2020-06-25 15:50:51 +0900

    WebApp4 수업예제

--- WebApp2/app.py
+++ WebApp2/app.py
@@ -9,16 +9,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-
     title = '파이썬 Flask 제어문 실습'
     content = ''
     dataset = {
         'title': '대한민국 도시',
         'cities': ['서울', '대전', '대구', '부산', '광주']
     }
+    return render_template('/register.html', tit=title, cont=content, data=dataset)
 
-    return render_template('/index.html', tit=title, cont=content, data=dataset)
-
+@app.route('/main')
+def main():
+    return render_template('/main.html')
 
 if __name__=='__main__':
     app.run()
